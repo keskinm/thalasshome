6176c0b4ec094110ed2c01ad0aba6239a0fe695c
Sasha Keskin <skeskin@bwi.earth>
2025-03-09 14:48:35 +0100

    fix

--- assets/my-date-picker.js
+++ assets/my-date-picker.js
@@ -1,33 +1,41 @@
 // --------------------- DATEPICKER custom-add--------------------- 
 $(document).ready(function() {
-    $.datepicker.regional['fr'] = {
-      closeText: 'Fermer',
-      prevText: 'Précédent',
-      nextText: 'Suivant',
-      currentText: 'Aujourd\'hui',
-      monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
-        'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
-      monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin',
-        'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
-      dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-      dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
-      dayNamesMin: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
-      weekHeader: 'Sem.',
-      dateFormat: 'yy-mm-dd',
-      firstDay: 1,
-      isRTL: false,
-      showMonthAfterYear: false,
-      yearSuffix: ''
-    };
+  function initializeDatePicker() {
+    if ($.datepicker) {
+      $.datepicker.regional['fr'] = {
+        closeText: 'Fermer',
+        prevText: 'Précédent',
+        nextText: 'Suivant',
+        currentText: 'Aujourd\'hui',
+        monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+          'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin',
+          'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+        dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
+        dayNamesMin: ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'],
+        weekHeader: 'Sem.',
+        dateFormat: 'yy-mm-dd',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+      };
+      
+      $.datepicker.setDefaults($.datepicker.regional['fr']);
     
-    $.datepicker.setDefaults($.datepicker.regional['fr']);
-  
-    $("#rental-start-date").datepicker({
-        minDate: 0
-    });
-  
-    $("#rental-end-date").datepicker({
-        minDate: 0
-    });
-  });
-  // --------------------- END DATEPICKER ---------------------
+      $("#rental-start-date").datepicker({
+          minDate: 0
+      });
+    
+      $("#rental-end-date").datepicker({
+          minDate: 0
+      });
+    } else {
+      setTimeout(initializeDatePicker, 100); // Retry after 100ms if not ready
+    }
+  }
+
+  initializeDatePicker();
+});
+// --------------------- END DATEPICKER ---------------------
